a616a5d2aa3ef2aaf5dcf11e8d898a85a98f3b67
Jahangir Hossain <jahangirhmarup@gmail.com>
2024-12-15 17:31:57 -0400

    Remove unused onClick prop from Link component

--- app/components/ui/Link.tsx
+++ app/components/ui/Link.tsx
@@ -9,7 +9,7 @@ interface LinkProps {
   onClick?: () => void;
 }
 
-const Link: React.FC<LinkProps> = ({ href, label, className="", onClick }) => (
+const Link: React.FC<LinkProps> = ({ href, label, className="" }) => (
   <a href={href} className={`block py-2 px-4 text-base md:text-lg ${className}`}>
     {label}
   </a>
