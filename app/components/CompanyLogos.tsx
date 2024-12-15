1bca73b9fc48c726c3d0c1e354d7adfebe263f0b
Jahangir Hossain <jahangirhmarup@gmail.com>
2024-12-15 17:25:07 -0400

    Fixed the Icons

--- app/components/CompanyLogos.tsx
+++ app/components/CompanyLogos.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const CompanyLogos = () => {
   return (
-    <div className="flex flex-wrap justify-center mx-4 md:mx-[40px] mt-8 gap-4">
+    <div className="flex flex-wrap justify-center mt-8 gap-[20px] md:gap-[60px]">
       {[
         { src: "/companies-logo/netflix.svg", alt: "Netflix", width: 100, height: 30 },
         { src: "/companies-logo/robin-hood.svg", alt: "Robinhood", width: 80, height: 30 },
@@ -10,7 +10,7 @@ const CompanyLogos = () => {
         { src: "/companies-logo/b-wear.svg", alt: "b.WEAR", width: 226, height: 40 },
         { src: "/companies-logo/priori.svg", alt: "Priori", width: 223, height: 60 },
       ].map((logo, index) => (
-        <div key={index} className="m-2 flex-shrink-0">
+        <div key={index} className="flex-shrink-0">
           <Image 
             src={logo.src} 
             alt={logo.alt} 
